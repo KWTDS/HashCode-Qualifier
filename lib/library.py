46bb1b82819355cd1d3c61ff0bd8594dc1453af4
Wahid Bawa <wahidbawa10@gmail.com>
2020-02-20 15:13:20 -0500

    objects are nice

--- lib/library.py
+++ lib/library.py
@@ -1,16 +1,15 @@
-
 from lib.book import Book
 
 
 class Library:
-    def __init__(self,id,signt,booksperday,books):
+    def __init__(self, id, signt, booksperday, books):
         self.id = id
         self.signt = signt
         self.booksperday = booksperday
         self.books = books
 
     def timescore(self):
-        self.signt + len(self.books)/self.booksperday
+        self.signt + len(self.books) / self.booksperday
 
     def bscore(self):
-        sum(map(lambda b: b.hscore()),self.books)
+        sum(map(lambda b: b.hscore(), self.books))
