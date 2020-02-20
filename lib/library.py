ddb3a759347af0c9c76856635e1db8339dc5aa53
Connor Horman <chorman64@gmail.com>
2020-02-20 14:41:43 -0500

    Add stuff

---
+++ lib/library.py
@@ -0,0 +1,16 @@
+
+from lib.book import Book
+
+
+class Library:
+    def __init__(self,id,signt,booksperday,books):
+        self.id = id
+        self.signt = signt
+        self.booksperday = booksperday
+        self.books = books
+
+    def timescore(self):
+        self.signt + len(self.books)/self.booksperday
+
+    def bscore(self):
+        sum(map(lambda b: b.hscore()),self.books)
